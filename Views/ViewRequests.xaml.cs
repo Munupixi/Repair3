76455908a6a88a2a47ff28567d1f3cb6a1fa01b7
Munupixi <driverufa12@gmail.com>
2024-05-03 18:38:24 +0500

    удадлил дерьмо

--- Views/ViewRequests.xaml.cs
+++ Views/ViewRequests.xaml.cs
@@ -28,7 +28,6 @@ namespace Repair3.Views
             FilterComboBox.ItemsSource =
                 repairContext.Statuses.Select(status => status.Title).ToList();
 
-            //requests = repairContext.Requests.ToList();
             requests = repairContext.Requests
                 .Include(r => r.Status)
                 .Include(r => r.Executor)
@@ -65,11 +64,6 @@ namespace Repair3.Views
             IssuedLabel.Content = viewRequests.Count();
             IssuedFromLabel.Content = requests.Count();
             RequestsDataGrid.ItemsSource = viewRequests;
-            //MainListView.Items.Clear();
-            //foreach (Request request in viewRequests)
-            //{
-            //    MainListView.Items.Add(new RequestUserControl(request));
-            //}
         }
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
