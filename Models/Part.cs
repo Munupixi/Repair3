ad4c544c3a3524dabe96353fc439087986449a48
Arturo Khairetdinov <driverufa12@gmail.com>
2024-05-03 12:02:03 +0500

    импортировал бд

---
+++ Models/Part.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repair3.Models;
+
+public partial class Part
+{
+    public int PartId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public float Cost { get; set; }
+
+    public int QuantityInStock { get; set; }
+
+    public virtual ICollection<UserOrder> UserOrders { get; set; } = new List<UserOrder>();
+}
---
+++ Models/UserOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repair3.Models;
+
+public partial class UserOrder
+{
+    public int UserOrderId { get; set; }
+
+    public string? Priority { get; set; }
+
+    public DateOnly CreationDate { get; set; }
+
+    public int RequestId { get; set; }
+
+    public int StatusId { get; set; }
+
+    public virtual Request Request { get; set; } = null!;
+
+    public virtual Status Status { get; set; } = null!;
+
+    public virtual ICollection<Part> Parts { get; set; } = new List<Part>();
+}
