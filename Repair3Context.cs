d966aa17d90f04a37090ae3716683e3b79ddc749
Adelina <missis.fazullina@yandex.ru>
2024-05-03 12:35:34 +0500

    пароль поменяли

--- Repair3Context.cs
+++ Repair3Context.cs
@@ -33,7 +33,7 @@ public partial class Repair3Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;password=Faza2005;database=Repair3", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=1234;database=Repair3", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
